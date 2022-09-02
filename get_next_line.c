6081f597f510cbcd76645e6e13e3b39f44b623e7
Mohamad Sharif <mosharif@lab1r2s4.42abudhabi.ae>
2022-09-02 21:48:26 +0400

    chagnes

--- get_next_line.c
+++ get_next_line.c
@@ -9,5 +9,14 @@
 /*   Updated: 2022/08/31 17:12:59 by mosharif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "get_next_line.h"
 
+char *get_next_line(int fd)
+{
+    int file;
+    int BUFFER_SIZE;
 
+    BUFFER_SIZE = 0;
+    // file = read(fd, BUFFER_SIZE, )
+    printf("%d", BUFFER_SIZE);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -10,4 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdint.h>
+
+
+#endif
