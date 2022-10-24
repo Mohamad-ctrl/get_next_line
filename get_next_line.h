f146e6d3103a5a3976d8a933633a70a335a66fe4
Mohamad <69107090+Mohamad-ctrl@users.noreply.github.com>
2022-10-24 15:34:37 +0400

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -12,14 +12,17 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# include <limits.h>
-# include <stdio.h>
 # include <stdlib.h>
-# include <string.h>
 # include <unistd.h>
-# include <stdarg.h>
-# include <stdint.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-
+char	*get_next_line(int fd);
+char	*ft_read_and_save(int fd, char *save);
+char	*ft_save(char *save);
+char	*ft_get_line(char *save);
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
 #endif
